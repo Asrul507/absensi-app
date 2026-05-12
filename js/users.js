64cc1e2142c7da23faba92c5dec347842e267a60
asrul <asrulmuh073@gmail.com>
2026-05-12 11:11:00 +0800

    Update users.js

--- js/users.js
+++ js/users.js
@@ -6,12 +6,14 @@ export async function getProfile(userId) {
     .from('profiles')
     .select('*')
     .eq('id', userId)
-    .maybeSingle() // 🔥 GANTI single()
+
+  console.log("PROFILE RAW:", data, error)
 
   if (error) {
     console.log('PROFILE ERROR:', error)
     return null
   }
 
-  return data
+  // 🔥 AMBIL DATA PERTAMA (AMAN)
+  return data?.[0] || null
 }
