b0ae46d02cc92bb15f1a5ace28c79d888d9bbb8e
asrul <asrulmuh073@gmail.com>
2026-05-12 07:09:30 -0400

    Update jadwal.js

--- js/jadwal.js
+++ js/jadwal.js
@@ -208,3 +208,8 @@ window.deleteJadwal = async function(id) {
 <button onclick="syncFromSheet()">
   🔄 Sync Jadwal Bulanan
 </button>
+window.syncFromSheet = async function () {
+
+  alert("Gunakan Google Apps Script untuk sync otomatis")
+
+}
