dbfa9b1d2a739aeba9000b82cfd1ff27d499dcb8
asrul <asrulmuh073@gmail.com>
2026-05-19 03:13:11 -0400

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,19 +1,16 @@
 import { supabase } from './supabase.js'
 
 export async function getProfile(userId) {
-
   const { data, error } = await supabase
     .from('profiles')
     .select('*')
     .eq('id', userId)
-
-  console.log("PROFILE RAW:", data, error)
+    .maybeSingle()
 
   if (error) {
-    console.log('PROFILE ERROR:', error)
+    console.error('getProfile error:', error)
     return null
   }
 
-  // 🔥 AMBIL DATA PERTAMA (AMAN)
-  return data?.[0] || null
+  return data
 }
