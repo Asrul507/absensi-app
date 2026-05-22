3c82261c36e110d504097739f2b7713a95c57c35
asrul <asrulmuh073@gmail.com>
2026-05-22 19:27:54 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,6 @@
 import { supabase } from './supabase.js'
 import { openCamera, takePhoto, getLocation, checkStatus } from './absensi.js'
 import { submitAbsen } from './submit_absensi.js'
-import { validateAbsenRadius } from './geolocation.js'
 
 window.activeVideoStream = null
 
